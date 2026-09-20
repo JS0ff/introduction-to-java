1a76219d63889f9ef69cea4c06bd71d19c5f9d2d
Asan Zhorobaev <onlyprogrammingjs@gmail.com>
2026-09-20 16:11:44 +0600

    add comments

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
     public static void main(String[] args){
 
+        // This is my first java program
+
+        /*
+            This
+            is
+            a
+            multi-line
+            comment
+         */
+
+
         System.out.println("Hello World!");
+
 
     }
 }
