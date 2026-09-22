740f5c6287b7d1cb201929b7abab6a2856c6d727
Asan Zhorobaev <onlyprogrammingjs@gmail.com>
2026-09-22 09:16:40 +0600

    Learn the arithmetic of java

---
+++ arithmetic/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args){
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+        System.out.println(result);
+
+
+//        z = x * y;
+//        z = x % y;
+//        System.out.println(z);
+
+//        x -= y;
+//        x *= y;
+//        x /= y;
+//        x %= y;
+
+
+    }
+
+}
